1b60d2ffb041987abc37767a752c6eba6a420691
4lov <101825262+4lov@users.noreply.github.com>
2022-03-24 17:30:59 +0300

    task

--- task4.py
+++ task4.py
@@ -2,6 +2,6 @@
 generator = [i for i in range(1, 20)]
 print(generator)
 #task 2
-def generator_function(generator: list) -> list:
-    new_generator = [i for i in range(1, 20)]
-    return new_generator
+def get_numbers_list(amount: list)-> list: 
+    return [i for i in range (amount + 1)]
+print(get_numbers_list(10))
