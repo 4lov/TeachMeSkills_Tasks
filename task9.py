7e368d49021b9b2fa79f677190e5e61ee2a51019
4lov <101825262+4lov@users.noreply.github.com>
2022-04-12 00:53:28 +0300

    task9

--- task9.py
+++ task9.py
@@ -1,26 +1,27 @@
 from dataclasses import dataclass
-import random
 
 @dataclass
 class user:
-    name: str
-    lastname: str
-    age: int
-    email: str
-    password: str
+    name: str = 'Aleksey'
+    lastname: str = 'Malachow'
+    age: int = 21
+    email: str = 'aleksey@gmail.com'
+    password: str = 213124
 
     @staticmethod
     def is_adolt(age):
         return age > 18
 
     @classmethod
-    def always_young(cls, age):
-        return cls(18)
+    def always_young(cls):
+        cls.age = 18
+        return cls.age
 
-user1 = user('Aleksey', 'Malachow', 21, 'aleksey@gmail.com', 213214)
+
+user1 = user()
 
 print(user1.age)
 
 print(user.is_adolt(21))
 
-#print(user.always_young())
+print(user.always_young())
